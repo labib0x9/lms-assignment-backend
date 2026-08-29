9df507972a4ee26dea0b09f287233ab1eb0e908e
Labib <labibfaisal450@gmail.com>
2026-08-29 14:19:47 +0600

    refactor(auth): remove redundant jwt service override in users-permissions

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,19 +1,6 @@
 'use strict';
 
 module.exports = (plugin) => {
-  const originalJwtService = plugin.services.jwt;
-
-  plugin.services.jwt = (context) => {
-    const service = originalJwtService(context);
-
-    return {
-      ...service,
-      issue(payload, jwtOptions) {
-        // console.log('JWT Payload:', payload);
-        return service.issue(payload, jwtOptions);
-      },
-    };
-  };
 
   const originalAuthController = plugin.controllers.auth;
 
@@ -33,22 +20,19 @@ module.exports = (plugin) => {
           });
 
           if (user && user.role) {
-            const roleType = user.role.type || user.role.name;
 
-            // 1. Attach role to user object in response
             ctx.body.user.role = {
               id: user.role.id,
               name: user.role.name,
               type: user.role.type,
             };
 
-            // 2. Sign JWT token with role included
             ctx.body.jwt = strapi
               .plugin('users-permissions')
               .service('jwt')
               .issue({
                 id: user.id,
-                role: roleType,
+                role: user.role.type,
               });
           }
         }
@@ -66,7 +50,6 @@ module.exports = (plugin) => {
         await controller.register(ctx);
 
         if (ctx.body && ctx.body.user && ctx.body.user.id) {
-          // 1. Find target role in database
           const targetRole = await strapi.db.query('plugin::users-permissions.role').findOne({
             where: {
               $or: [
@@ -76,7 +59,6 @@ module.exports = (plugin) => {
             },
           });
 
-          // 2. Assign role & set blocked: true for non-students (requires admin approval)
           await strapi.db.query('plugin::users-permissions.user').update({
             where: { id: ctx.body.user.id },
             data: {
@@ -85,7 +67,6 @@ module.exports = (plugin) => {
             },
           });
 
-          // 3. Fetch updated user
           const user = await strapi.db.query('plugin::users-permissions.user').findOne({
             where: { id: ctx.body.user.id },
             populate: ['role'],
@@ -99,18 +80,15 @@ module.exports = (plugin) => {
             };
           }
 
-          // 4. Handle JWT token & response message based on role
           if (isStudent) {
-            // Students receive JWT immediately for direct dashboard access
             ctx.body.jwt = strapi
               .plugin('users-permissions')
               .service('jwt')
               .issue({
                 id: user.id,
-                role: user.role?.type || user.role?.name,
+                role: user.role?.type,
               });
           } else {
-            // Instructors / Admins / Content Managers must wait for admin approval
             delete ctx.body.jwt;
             ctx.body.message =
               'Registration submitted! Your account is pending administrator approval before you can log in.';
