c03945b896461973e653dc85a06ea62f48379b8d
Labib <labibfaisal450@gmail.com>
2026-08-30 14:05:08 +0600

    Restrict CORS to Vercel frontend origin instead of default wildcard

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,15 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'https://lms-assignment-frontend-mu.vercel.app',
+      ],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
